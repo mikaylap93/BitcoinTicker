99dccbe81100e5f95ec97c3e5f261a73a7db7fda
mikaylap93 <74144308+mikaylap93@users.noreply.github.com>
2021-06-04 23:42:34 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const http = require("http");
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var weekPrices = [0,0,0,0,0,0,0];
+var priceGraphProportions=[0,0,0,0,0,0,0];
+var weekDates = [0,0,0,0,0,0,0];
+
+const bpiHistPriceURL = "http://api.coindesk.com/v1/bpi/historical/close/USD.json";
+
+app.get("/",function(req,res){
+
+//GET HISTORICAL PRICES
+http.get(bpiHistPriceURL,function(histresponse){
+
+//Buffer in chunks to avoid unexpected end error
+  var chunks = [];
+  histresponse.on('data',function(data){
+    chunks.push(data);}).on('end',function() {
+      var data = Buffer.concat(chunks);
+      var histPriceData = JSON.parse(data);
+
+      var histPriceArr = Object.values(histPriceData.bpi);
+      var histDatesArr = Object.keys(histPriceData.bpi);
+
+      //pull and set weekprices & dates from histPriceData api object
+      count=24;
+      for(i=0;i<7;i++){
+        weekPrices[i] = Math.round(histPriceArr[count]/1000);
+        weekDates[i] =splitAndFormatDate(histDatesArr[count]);
+        priceGraphProportions[i] =(weekPrices[i].toString())[1];
+        count++;
+      }
+  });
+});
+  //Send data
+  res.render('index',{
+  day1Price:weekPrices[0],tuesPrice:weekPrices[1],wedPrice:weekPrices[2],thursPrice:weekPrices[3],
+  friPrice:weekPrices[4],satPrice:weekPrices[5],sunPrice:weekPrices[6],
+  pGP1:priceGraphProportions[0],pGP2:priceGraphProportions[1],pGP3:priceGraphProportions[2],
+  pGP4:priceGraphProportions[3],pGP5:priceGraphProportions[4],pGP6:priceGraphProportions[5],
+  pGP7:priceGraphProportions[6],
+  day1:weekDates[0],day2:weekDates[1],day3:weekDates[2],day4:weekDates[3],day5:weekDates[4],day6:weekDates[5],day7:weekDates[6]});
+
+});
+
+function splitAndFormatDate(date){
+  var day = date.substring(7,9);
+  var mon = date.substring(8,11);
+  if(date.substring(5,6)==0){day = date.substring(6,7);}
+  if(date.substring(8,9)==0){mon = date.substring(9,10);}
+
+  formDate = day+"/"+mon
+  return formDate;
+}
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
---
+++ public/index.js
@@ -0,0 +1,99 @@
+
+var timer = null;
+var previousUsdPrice = 0;
+var usdPrice = 0;
+var eurPrice =0;
+var gbpPrice = 0;
+
+//force data to load completly before modifying
+$(document).ready(function() {
+
+  if (document.URL.indexOf("#") == -1) {
+    url = document.URL + "#loaded";
+    location = "#loaded";
+    location.reload(true);
+
+  }
+tickerAutoReloader(15);
+
+  $("#dark-mode-toggle").change(function() {
+    if (document.querySelector("#dark-mode-toggle").checked == true) {
+      document.querySelector(".css-styles").href = "../drkStyles.css";
+    } else {
+      document.querySelector(".css-styles").href = "../styles.css";
+    }
+  })
+
+  $("#sec-15").change(function() {
+    var seconds = parseFloat(($("#sec-15").val()));
+    clearTimeout(timer);
+    tickerAutoReloader(seconds);
+  });
+
+  $("#sec-30").change(function() {
+    var seconds = parseFloat(($("#sec-30").val()));
+    clearTimeout(timer);
+    tickerAutoReloader(seconds);
+
+  });
+
+  $("#sec-60").change(function() {
+    var seconds = parseFloat(($("#sec-60").val()));
+    clearTimeout(timer);
+    tickerAutoReloader(seconds);
+  });
+
+  function reloadTickersFromAPI() {
+    var currentPriceUrl = "http://api.coindesk.com/v1/bpi/currentprice.json";
+    //Get the current prices
+    $.ajax({
+      method: "GET",
+      url: currentPriceUrl,
+    })
+    .done(function(res){
+      var currentPriceObject = JSON.parse(res);
+
+      usdPrice = currentPriceObject.bpi.USD.rate;
+      eurPrice = currentPriceObject.bpi.EUR.rate;
+      gbpPrice = currentPriceObject.bpi.GBP.rate;
+
+      var ticker = findCurrentPriceTick(previousUsdPrice,usdPrice);
+      if(ticker=='▲'){
+        $("#usd-ticker-price").css("color","green");
+      }else if(ticker=='▼'){
+        $("#usd-ticker-price").css("color","#C70039");
+      }else $("#usd-ticker-price").css("color","black");
+
+      $("#usd-ticker-price").text(usdPrice + ticker);
+      $("#eur-ticker-price").text(eurPrice);
+      $("#gbp-ticker-price").text(gbpPrice);
+
+    })
+    .fail(function(){
+      console.log("fail");
+    });
+    previousUsdPrice = usdPrice;
+
+  }
+
+  function findCurrentPriceTick(previousPrice,currentPrice){
+
+      if(currentPrice>previousPrice){
+        console.log(currentPrice+" "+previousPrice);
+        tick = '▲';
+    } else if(currentPrice<previousPrice){
+        tick = '▼';
+    } else tick = '';
+    return tick;
+
+  }
+
+  function tickerAutoReloader(seconds) {
+    reloadTickersFromAPI();
+    console.log("reloadedTickers after " + seconds + " seconds");
+    timer = setTimeout(function() {
+      tickerAutoReloader(seconds)
+    }, (seconds * 1000));
+  };
+
+});
